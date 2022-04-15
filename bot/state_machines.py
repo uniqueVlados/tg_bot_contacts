53da820bfa09d656d3a9f9341ee96bde0ed36cc7
Andrew <samysenko_a@mail.ru>
2022-04-15 18:54:28 +0300

    + StateMachine (small refactor)

--- bot/state_machines.py
+++ bot/state_machines.py
@@ -1,17 +1,10 @@
 from os.path import exists as file_exists
-from json import load as json_load, dumps as json_dumps
+from json import load as json_load, dump as json_dump
 
 
 class StateMachine:
-    ENCODING = "UTF-8"
-
-    def __init__(self, filename="state_db.json"):
-        self.dbname = filename
-
-        self.states = {}
-        if file_exists(filename):
-            self.read_database(filename)
-        self.__doc__ = """
+    _ENCODING = "UTF-8"
+    __doc__ = """
         --------- EXAMPLE ---------
         
         states = StateMachine()
@@ -19,21 +12,26 @@ class StateMachine:
         user_current_state = states.get(121241)
         if user_current_state is not None:
             do_some_actions()
-    """
+        """
+
+    def __init__(self, filename="state_db.json"):
+        self._dbname = filename
+
+        self._states = {}
+        if file_exists(filename):
+            self._read_database(filename)
 
     def get(self, user_id):
-        return self.states.get(str(user_id), None)
+        return self._states.get(str(user_id), None)
 
     def add(self, user_id, state):
-        self.states[str(user_id)] = state
-        self.update_database()
+        self._states[str(user_id)] = state
+        self._update_database()
 
-    def update_database(self):
-        with open(self.dbname, "w", encoding=StateMachine.ENCODING) as file:
-            json_dumps(file)
+    def _update_database(self):
+        with open(self._dbname, "w", encoding=StateMachine._ENCODING) as file:
+            json_dump(self._states, file)
 
-    def read_database(self, filename):
-        with open(filename, "r", encoding=StateMachine.ENCODING) as file:
-            self.states = json_load(file)
-
-
+    def _read_database(self, filename):
+        with open(filename, "r", encoding=StateMachine._ENCODING) as file:
+            self._states = json_load(file)
