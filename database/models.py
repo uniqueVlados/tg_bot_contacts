d2f59b819e7842d3c3d67963694d94b6fbdaa22d
Andrew <samysenko_a@mail.ru>
2022-06-23 23:28:26 +0300

    +State table

--- database/models.py
+++ database/models.py
@@ -16,13 +16,12 @@ class User(Base):
     tg_user_id = Column(String(15))
     description = Column(Text(200))
     location = Column(String(50))
-    last_use = Column(Date)
     # is_active = Column(Boolean)
-    state = Column(String(30))
     invite_code = Column(String(10))
 
     likes_from = relationship("Like", back_populates="user_from", foreign_keys="Like.from_user_id")
     likes_to = relationship("Like", back_populates="user_to", foreign_keys="Like.to_user_id")
+    state = relationship("State", back_populates="user", foreign_keys="State.user_id")
 
     def __repr__(self):
         return f"<User({self.id} {self.name},{self.last_name})>"
@@ -39,4 +38,15 @@ class Like(Base):
 
     def __repr__(self):
         return f"<Like({self.subject} to {self.object})>"
+
+
+class State(Base):
+    __tablename__ = 'like'
+    tg_id = Column(Integer, nullable=False, unique=True, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    value = Column(String(10))
+    user = relationship("User", back_populates="state")
+
+    def __repr__(self):
+        return f"<State({self.tg_id} {self.value})>"
 
--- database/operations.py
+++ database/operations.py
@@ -1,7 +1,5 @@
-from sqlalchemy.orm import Session
-
 from . import get_db
-from database.models import User, Like
+from database.models import User, Like, State
 
 
 def create_like(from_user_id: int, to_user_id: int):
@@ -28,4 +26,16 @@ def get_user_likes(user_id: int):
     """ Возвращает все пользователей, которые лайкнули пользователя """
     db = get_db()
     return db.query(Like).filter(Like.to_user_id == user_id).all()
+
+
+def create_user(params):
+    """ Создает пользователя в бд """
+    user = User(**params)
+    db = get_db()
+    db.add(user)
+
+    state = db.query(State).filter(State.user_id == user.id).first()
+    state.user_id = user.id
+
+    db.commit()
 
