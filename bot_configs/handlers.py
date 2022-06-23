11bd951d1374564a1a202ad158246ffbe960068d
Andrew <samysenko_a@mail.ru>
2022-06-23 21:56:52 +0300

    +handler +states

--- bot_configs/handlers.py
+++ bot_configs/handlers.py
@@ -2,9 +2,22 @@ from database.operations import create_like
 from . import dp
 from .messages import *
 from aiogram import types
+from states import *
 
 
 @dp.message_handler(commands=['start'])
 async def start_(message: types.Message):
-    await message.reply(WELCOME)
-    create_like()
+    user_id = message.from_user.id
+
+    has_account = check_user_account(user_id)
+    if not has_account:
+        reply_text = WELCOME
+        user_new_state = NEED_INVITE
+
+    reply_text = WELCOME_HAS_ACCOUNT
+    user_new_state = WAIT_FOR_ACTION
+
+    await message.reply(reply_text)
+    await change_user_state(user_id, user_new_state)
+
+
